eb41054b6e00711dfed84df32766c6b75711f31e
lawlerblader <jacob@hamblin.cc>
2015-04-08 02:44:20 -0700

    navbar dropdown fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
   get 'session/delete', :to => 'sessions#delete'
+  get 'users/session/delete', :to => 'sessions#delete'
 end
